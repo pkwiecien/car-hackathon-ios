5731f00758bd3cb7f9e73a71b970e2b4694c34c0
Pawel Kwiecien <p.kwiecien@tum.de>
2014-05-16 23:13:45 +0200

    Initial commit

--- car-hackathon/AppDelegate.m
+++ car-hackathon/AppDelegate.m
@@ -7,13 +7,17 @@
 //
 
 #import "AppDelegate.h"
+#import "MainViewController.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+
+    MainViewController *mainVC = [[MainViewController alloc] init];
+    self.window.rootViewController = mainVC;
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
---
+++ car-hackathon/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  car-hackathon
+//
+//  Created by Pawel Kwiecien on 5/16/14.
+//  Copyright (c) 2014 Pawel Kwiecien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
